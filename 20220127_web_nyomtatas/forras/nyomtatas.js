531853c116466da0271d893fe8a5b1331b61f36f
Korpa Gábor Máté <korpa.gabor@sulinet.csmszki>
2022-01-27 14:05:54 +0100

    ..

--- 20220127_web_nyomtatas/forras/nyomtatas.js
+++ 20220127_web_nyomtatas/forras/nyomtatas.js
@@ -1,11 +1,16 @@
 ﻿function kalkulal(){
     //Űrlapadatok
-    const szelesseg=297;
-    const magassag=420;
+    const szelesseg=document.getElementById('szelesseg').value;
+    const magassag=document.getElementById('magassag').value;
     const papir=document.getElementById('papirtipus').value;
     //Számítások
     let terulet=Math.round((szelesseg*magassag)/10000);    
     let koltseg=terulet*papir;
+
+    document.getElementById('terulet').innerHTML=terulet
+    document.getElementById('papir').innerHTML=koltseg
+    document.getElementById('koltseg').innerHTML=terulet*koltseg
+
     //Megjelenítés
     document.getElementById('valasz').style.visibility = "visible";
 }
